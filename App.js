82436cf1458969110786af892c41863e0675bde0
Ryu1011 <jerome1011@naver.com>
2023-10-15 19:55:25 +0900

    init

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
+import React, { useState, useEffect, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Camera } from 'expo-camera'
+
 
 export default function App() {
+  const viewRef = useRef();
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.front);
+  const [camera, setCamera] =  useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === 'granted');
+    })();   
+}, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View 
+    style={{width:'100%', height:'100%', alignItems: 'center', marginTop:100}}>
+      <Camera
+      style={{width:'80%', height:'80%'}}
+      type={type}
+      ratio={'1:1'}/>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
