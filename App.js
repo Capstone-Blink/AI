b9eedefb3842ef835eac2f2e20fa5e0ea80b3497
김동욱 <bradl0809@naver.com>
2023-10-17 16:37:18 +0900

    tfjs model

--- App.js
+++ App.js
@@ -1,38 +1,150 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Camera } from 'expo-camera'
-
+import { Camera } from 'expo-camera';
+import * as tf from '@tensorflow/tfjs';
+import { bundleResourceIO } from '@tensorflow/tfjs-react-native';
+import { cameraWithTensors } from '@tensorflow/tfjs-react-native';
+import '@tensorflow/tfjs-react-native';
+import '@tensorflow/tfjs-backend-webgl';
 
 export default function App() {
-  const viewRef = useRef();
-  const [hasCameraPermission, setHasCameraPermission] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.front);
-  const [camera, setCamera] =  useState(null);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [model, setModel] = useState(null);
 
   useEffect(() => {
     (async () => {
-      const cameraStatus = await Camera.requestCameraPermissionsAsync();
-      setHasCameraPermission(cameraStatus.status === 'granted');
-    })();   
-}, []);
+      try{
+        // Request camera permission
+        const { status } = await Camera.requestCameraPermissionsAsync();
+        setHasPermission(status === 'granted');
+
+        // Initialize TensorFlow.js
+        await tf.ready();
+
+        // Load the model
+        const modelJson = require('./assets/model.json');
+        const modelWeights1 = require('./assets/group1-shard1of3.bin');
+        const modelWeights2 = require('./assets/group1-shard2of3.bin');
+        const modelWeights3 = require('./assets/group1-shard3of3.bin');
+        const model = await tf.loadGraphModel(bundleResourceIO(modelJson, [modelWeights1, modelWeights2, modelWeights3]));
+        setModel(model);
+        console.log("Model Loaded Successfully");
+      } catch (err) {
+        error(err);
+      }
+    })();
+  }, []);  
+
+  const THRESHOLD = 0.5;
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+
+
+  async function getLabelIndices(modelOutput, confidenceThreshold = 0.9) {
+    // 1. Extract the confidence scores from the model's output
+    const confidenceScores = tf.slice(modelOutput, [0, 4, 0], [1, 1, -1]).squeeze();
+
+    // 2. Identify the bounding boxes that have confidence scores above the threshold
+    const detectedBoxes = tf.where(confidenceScores.greater(confidenceThreshold));
+
+    // 3. For each detected box, extract the class probabilities and identify the label index
+    const labelIndices = [];
+    const detectedBoxesArray = await detectedBoxes.array();
+
+    for (let i = 0; i < detectedBoxesArray.length; i++) {
+        const idx = detectedBoxesArray[i];
+        const classProbabilities = tf.slice(modelOutput, [0, 5, idx], [1, 4, 1]).squeeze();
+        const maxClassIdx = classProbabilities.argMax().dataSync()[0];
+        labelIndices.push(maxClassIdx);
+    }
+
+    return labelIndices;
+  }
+
+  
+  const handleCameraStream = (images, updatePreview, gl) => {
+    const loop = async () => {
+      try{
+
+        const imageTensor = images.next().value;
+        
+
+        if (imageTensor == null) {
+          requestAnimationFrame(loop);
+          return;
+        }
+
+        const imageTensorFloat = imageTensor.toFloat();
+        const batchedImage = imageTensorFloat.expandDims(0);
+        
+       
+        if(model != null){
+          const predictions = model.predict(batchedImage);
+          console.log(predictions);
+          const labels = await getLabelIndices(predictions);
+          // console.log(labels);
+        }
+        tf.dispose([imageTensor, imageTensorFloat, batchedImage]);
+        // Update preview
+        requestAnimationFrame(loop);
+      } catch (error) {
+        console.log("Error in handle loop : ", error);
+      }
+
+    };
+    loop();
+  };
+
+  const TensorCamera = cameraWithTensors(Camera);
+
+
+  let textureDims;
+  if (Platform.OS === 'ios') {
+   textureDims = {
+     height: 1920,
+     width: 1080,
+   };
+  } else {
+   textureDims = {
+     height: 1200,
+     width: 1600,
+   };
+  }
 
   return (
-    <View 
-    style={{width:'100%', height:'100%', alignItems: 'center', marginTop:100}}>
-      <Camera
-      style={{width:'80%', height:'80%'}}
-      type={type}
-      ratio={'1:1'}/>
+    <View style={styles.container}>
+      <TensorCamera
+        style={styles.camera}
+        type={Camera.Constants.Type.back}
+        onReady={handleCameraStream}
+        autorender={false}
+              // Standard Camera props
+        // Tensor related props
+        cameraTextureHeight={textureDims.height}
+        cameraTextureWidth={textureDims.width}
+        resizeDepth={3}
+        resizeHeight={416}
+        resizeWidth={416}
+        
+        dtype
+      >
+      </TensorCamera>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  camera: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    zIndex: 1
   },
 });
---
+++ metro.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+  resolver: {
+    assetExts: ['bin', 'txt', 'jpg', 'png', 'json', 'xml', 'wav', 'mp3', 'gif'], 
+  },
+};
